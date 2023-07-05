fc2b5b5561354fb51e4dfb539d7646436e557a8d
Rajeev Chapagain <rajeevchapagain01@gmail.com>
2023-07-05 16:05:18 -0500

    api links are updated

--- src/utilities/loadAuthenticationData.js
+++ src/utilities/loadAuthenticationData.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-const AUTHENTICATION_LOGIN_URL = "http://localhost:8080/auth/user/login";
+const AUTHENTICATION_LOGIN_URL =
+  "https://employee-management-api-production-af0d.up.railway.app/auth/user/login";
 const AUTHENTICATION_FINDEMPLOYEE_URL = "/employee";
-const USER_REGISTER_URL = "http://localhost:8080/auth/user/register";
-const API_URL = "http://localhost:8080";
+const USER_REGISTER_URL =
+  "https://employee-management-api-production-af0d.up.railway.app/auth/user/register";
+const API_URL =
+  "https://employee-management-api-production-af0d.up.railway.app/";
 
 export const loadAUthenticationData = async (userName, password) => {
   try {
@@ -60,7 +63,9 @@ export const createAxios = (token) => {
 
 export const loadAccountUserData = async (token) => {
   try {
-    const response = await axios.get("http://localhost:8080/auth/user");
+    const response = await axios.get(
+      "https://employee-management-api-production-af0d.up.railway.app/auth/user"
+    );
 
     return response.data;
   } catch (error) {}
@@ -69,7 +74,7 @@ export const loadAccountUserData = async (token) => {
 export const deleteUserAccount = async (id) => {
   try {
     const response = await axios.delete(
-      `http://localhost:8080/auth/user/remove/${id}`
+      `https://employee-management-api-production-af0d.up.railway.app/auth/user/remove/${id}`
     );
   } catch (error) {
     console.log(error);
